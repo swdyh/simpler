b9ff38df28414f718d29500a517d5d98c4b565cb
swdyh <youhei@gmail.com>
2017-07-30 12:26:02 +0900

    remove release script

--- release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-v=$(cat  package.json | ruby -rjson -e 'puts JSON.parse(ARGF.read)["version"]')
-f="simplenote-electro-darwin-x64-$v.zip"
-cd packages/$v/darwin-x64
-zip -r $f SimplenoteElectro.app
-mv $f ../../../
-cd ../../../
-hub release create -m "$v" -a $f $v
-rm $f
